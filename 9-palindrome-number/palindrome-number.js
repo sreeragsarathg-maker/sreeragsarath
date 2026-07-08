531332767fc8e3d8a7f19c00de391775e878f380
sreeragsarathg-maker <sreeragsarathg@gmail.com>
2026-07-08 20:32:49 +0530

    Time: 3 ms (98.12%) | Memory: 62.8 MB (92.48%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,16 @@
+function isPalindrome(x) {
+    if (x < 0) {
+        return false;
+    }
+
+    let original = x;
+    let reversed = 0;
+
+    while (x > 0) {
+        let digit = x % 10;
+        reversed = reversed * 10 + digit;
+        x = Math.floor(x / 10);
+    }
+
+    return original === reversed;
+}
