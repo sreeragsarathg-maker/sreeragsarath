36f967ff24803134e645d5723b45c61ef7c1eb48
sreeragsarathg-maker <sreeragsarathg@gmail.com>
2026-07-17 00:05:12 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (53.80%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,14 @@
+function lengthOfLastWord(s) {
+    let length = 0;
+    let i = s.length - 1;
+
+    while (i >= 0 && s[i] === " ") {
+        i--;
+    }
+    while (i >= 0 && s[i] !== " ") {
+        length++;
+        i--;
+    }
+
+    return length;
+}
