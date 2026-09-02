07dd25eb721718ad9d656575ef4218d3019069b1
sreeragsarathg-maker <sreeragsarathg@gmail.com>
2026-09-02 17:04:41 +0530

    Time: 2 ms (20.65%) | Memory: 58.6 MB (19.68%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -1,14 +1,14 @@
-function removeDuplicates(nums) {
-    if (nums.length === 0) return 0;
-
-    let i = 0;
-
-    for (let j = 1; j < nums.length; j++) {
-        if (nums[i] !== nums[j]) {
-            i++;
-            nums[i] = nums[j];
-        }
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i=0
+    for(j=1;j<nums.length;j++){
+         if(nums[j]!==nums[i]){
+            i++
+            nums[i]=nums[j]
+         }
     }
-
-    return i + 1;
-}
+    return i+1
+};
