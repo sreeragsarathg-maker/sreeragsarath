416ea97b0667cfa6a30020ea06f71804c20a8127
sreeragsarathg-maker <sreeragsarathg@gmail.com>
2026-09-02 10:38:40 +0530

    Time: 68 ms (10.15%) | Memory: 53.6 MB (93.14%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -5,10 +5,11 @@
  */
 var twoSum = function(nums, target) {
     for(i=0;i<nums.length;i++){
-    for(j=0;j<nums.length;j++){
-        if(nums[i]+nums[j]===target && i!=j){
-            return [i,j];
+        for(j=i+1;j<nums.length;j++){
+            if(nums[i] + nums[j]==target){
+                return [i,j]      
+            }
         }
     }
-    }
+
 };
