1aca35157d3d8db40d05d90330e333fd03be4d98
sreeragsarathg-maker <sreeragsarathg@gmail.com>
2026-09-03 10:23:14 +0530

    Time: 0 ms (100.00%) | Memory: 55.5 MB (5.30%) - LeetSync

--- 35-search-insert-position/search-insert-position.js
+++ 35-search-insert-position/search-insert-position.js
@@ -4,11 +4,10 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-      for(i=0;i<=nums.length;i++){
-        if(nums[i]>=target){
-            return i;
-        }
-
-      }
-    return nums.length;
+for(i=0;i<nums.length;i++){
+  if(nums[i]>=target){
+    return i
+  }
+}
+return nums.length
 }
